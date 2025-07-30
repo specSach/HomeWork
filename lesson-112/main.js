6d17ab5b60821916c74c51dd83a76931eec4a7ae
sach <romakarpow4@gmail.com>
2025-07-30 21:41:01 +0300

    Update main.js

--- lesson-112/main.js
+++ lesson-112/main.js
@@ -1,6 +1,10 @@
 const btn = document.querySelector('.btn-open')
 const modal = document.querySelector('.modal')
 
+const closeModal = () => {
+  modal.classList.remove('modal--open')
+}
+
 btn.addEventListener('click', () => {
   modal.classList.add('modal--open')
 })
@@ -9,11 +13,13 @@ modal.addEventListener('click', event => {
   const target = event.target
   if (target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
     modal.classList.remove('modal--open')
+    closeModal()
   }
 })
 
 document.addEventListener('keydown', event => {
   if (event.code === 'Escape' && modal.classList.contains('modal--open')) {
     modal.classList.toggle('modal--open')
+    closeModal()
   }
 })
